9e899b0d99e60e5e65cde2a580e1498457e3e243
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-06-01 18:44:57 -0500

    Data Type Example

---
+++ Python Data Type.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 23)
+
+# Integer
+y = -2015
+print("Integer:", y)
+
+# Float
+z = 15.9
+print("Float:", z)
+
+# String
+name = "Shaq"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
