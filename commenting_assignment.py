86ae7e2bcae922bbd2ded989264383538e43a634
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-06-01 20:51:06 -0500

    Commenting Assignment Updated

---
+++ commenting_assignment.py
@@ -0,0 +1,7 @@
+tank_size = 15 #Total Tank Size of Vehicle
+# Define the total distance covered
+distance = 600 #Total Distance able to be traveled on Full Tank
+# Calculate miles per gallon
+mpg = distance / tank_size #Miles per Gallon Equation
+# Print the result
+print("Miles per gallon:", mpg) #Prints the Miles per Gallon expected
