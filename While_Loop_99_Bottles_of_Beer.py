0abb59e2d5303b4c236f5774648101ae81d77a0a
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-06-07 09:37:11 -0500

    99 Bottles of Beer on the Wall While Loop Project

---
+++ While_Loop_99_Bottles_of_Beer.py
@@ -0,0 +1,7 @@
+count = 99
+while count > 0:
+    print(count, "Bottles of Beer on the Wall")
+    print(count, "Bottles of Beer")
+    print("Take one Down and Pass It around")
+    count -= 1  # Increment count
+    print(count, "Bottles Of Beer on the wall")
