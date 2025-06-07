f627b70255a2cb8ce5c613a4cfe23244bc8bb59d
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-06-07 15:12:51 -0500

    Showcase of Use of basic logic operations

---
+++ Basic_Logic_Operations.py
@@ -0,0 +1,8 @@
+value1 = int(input("Input a Number?")) #integer  1 input
+value2 = int(input("Input a Number?")) #integer  2 input
+if(value1 >0 and value2 >0):
+    print("Both values are greater than zero")
+else:(print("Both values are not greater than zero"))
+if(value1 >100 and value2 >100):
+    print("Both values are greater than 100")
+else:(print("Both values are not greater than 100"))
