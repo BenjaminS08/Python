e824927ec695cc63823bc4d767d87431b090e2ce
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-07-20 21:52:15 -0500

    Using Try, Except and Finally

--- Exceptions.py
+++ Exceptions.py
@@ -1,11 +1,28 @@
-# Simple Python program to calculate the square of a number with error handling
+# Custom exception class
+class NotNumericError(Exception):
+    def __init__(self, message="Input is not a numeric value."):
+        super().__init__(message)
 
-def square_number():
-    try: #Tests to see if the input is a number or not
-        number = input("Enter a number to square: ")
-        squared_number = int(number) ** 2
-        print(f"The square of {number} is {squared_number}.")
-    except ValueError:
-        print("Please enter a valid number.")
 
-square_number()
+def get_numeric_input():
+    while True:  # While true loop that loops until a valid input is entered
+        try:
+            user_input = input("Enter a number: ")
+
+            if not user_input.isnumeric():
+                raise NotNumericError(
+                    "Error: You must enter a number.")
+
+        except NotNumericError as e:
+            print(e)  # Print custom error message
+
+        else:
+            print(f"You entered the number: {user_input}")
+            break  # Exit loop if input is valid
+
+        finally:
+            print("End of input attempt.\n")
+
+
+# Run the program
+get_numeric_input()
