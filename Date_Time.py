d9862be2eccc15da29f1fddb2b5149571bc7fde6
BenjaminS08 <bstandish997@students.mchenry.edu>
2025-08-02 12:09:41 -0500

    Calculates how old you are in Days, Months, Weeks, and Years.

---
+++ Date_Time.py
@@ -0,0 +1,35 @@
+# calculate a person's age in: Years, months, weeks, days
+# 30.41 days in a month
+# 7 days in a week 
+
+from datetime import datetime
+
+def main():
+   
+    print("\n\n")
+    try:
+        today = datetime.today()
+        birth_year = int(input("What year were you born?  "))
+        month = int(input("What Month were you born (as a number. May would be 5)  "))
+        day = int(input("What day of the month were you born?  "))
+        # just need datetime not datetime.date
+        # because we imported datetime from datetime
+        birthday = datetime(birth_year, month, day)
+        print("Your birthday is: ")
+        birthday_output = birthday.strftime("%Y-%m-%d") #Formatting Birthday
+        print(birthday_output) 
+
+        delta = today - birthday
+        print(f'Difference is {delta.days} days')
+        delta_years = delta.days // 365.2425 #Calculates Days old
+        delta_months = delta_years * 12 #Calculates the Years old
+        delta_weeks = delta_months * 4.34524 #Calculates Months old
+       #Print Statements for Each Calculation
+        print(f'You are {delta_years} years old')
+        print(f'You are {delta_months} months old')
+        print(f'You are {delta_weeks} weeks old')
+      
+    except Exception as e:
+        print(f"ooooops!!!:  {e}") 
+        main()
+main()
